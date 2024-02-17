eafb6d50e14b01cbcd034ef5051899b92f5edb02
nepali <npoorna09@gmail.com>
2024-02-17 13:41:34 +0100

    first commit

---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,25 @@
+import org.example.Main;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestCalculator {
+
+
+    public Main main;
+    @Test
+    public void testMainConstrctor()
+    {
+        main =new Main();
+
+        assertNotEquals(main,null);
+    }
+    @Test
+    public void testSub()
+    {
+        main=new Main();
+        assertEquals(0,main.sub(2,2));
+    }
+
+
+}
