3dae358505b4dd1a6f78e025b2cefb8078daa1ed
jeevanthipanawala <jeevanthi.panawala@gmail.com>
2024-02-19 16:44:41 +0100

    Added Logarithm function and Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,9 +8,31 @@ public class Main {
         System.out.println("Hi welcome to calculator");
 
         }
-
         public int add(int a ,int b){return a+b;}
     public int sub(int a,int b){return a-b;}
     public int multi(int a,int b){return a*b;}
+    public int division(int a,int b){return a/b;}
+
+    public int modulus(int a,int b){return a % b;}
+
+    public int squareroot(int a){return (int) Math.sqrt(a);}
+    public int absoluteValue(int a){return (int) Math.abs(a);}
+
+    public int maxValue(int a,int b){return Math.max(a,b);}
+    public int minValue(int a,int b){return Math.min(a,b);}
+
+    public int  rountvalue(float a){return (int) Math.round(a);}
+
+    //Logarithm - Jeevanthi
+    public  float Logarithm(float a)
+    {
+        return (float) Math.log(a);
     }
+
+
+
+
+
+    }
+
 
--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -27,4 +27,60 @@ public class TestCalculator {
         assertEquals(4,main.multi(2,2));
     }
 
+    @Test
+    public void testdivision() // test division
+    {
+        main=new Main();
+        assertEquals(1,main.division(2,2));
+    }
+    @Test
+    public void testmodulus() // test modulus
+    {
+        main=new Main();
+        assertEquals(1,main.modulus(3,2));
+    }
+    @Test
+    public void testsqurt() // test sqrt
+    {
+        main=new Main();
+        assertEquals(2,main.squareroot(4));
+    }
+
+    @Test
+    public void testabsoluteValue() // test absoluteValue
+    {
+        main=new Main();
+        assertEquals(3,main.absoluteValue(3));
+    }
+    @Test
+    public void testmaxValue() // test max value
+    {
+        main=new Main();
+        assertEquals(8,main.maxValue(7,8));
+    }
+
+    @Test
+    public void testminValue() // test min value
+    {
+        main=new Main();
+        assertEquals(5,main.minValue(6,5));
+    }
+    @Test
+    public void testrountvalue() // test round value
+    {
+        main=new Main();
+        assertEquals(3,main.rountvalue(2.8F));
+    }
+
+    @Test
+    public void TestLogarithm()
+    {
+        main=new Main();
+        assertEquals(1,main.Logarithm(10.0F),0.0F);
+    }
+
+
+
+    
+
 }
