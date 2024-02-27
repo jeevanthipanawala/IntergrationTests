5374c08ca9a9357a8a752ba2de3b7d3461959ae6
Indika Eranga <indikaeranga06@gmail.com>
2024-02-27 10:56:37 +0100

    Add integration test case to separate class

--- src/test/java/ITCalculator.java
+++ src/test/java/ITCalculator.java
@@ -2,9 +2,38 @@
 import org.example.Main;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.Assert.*;
 
 public class ITCalculator {
 
+    private Main main;
+
+    @BeforeEach
+    void setup() {
+        main = new Main();
+    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/TestData_AdditionAndMultiplication.csv")
+    public void testAdditionAndMultiplication(int mulParam1, int mulParam2, int addParam, int expectedResult) {
+
+        // Perform the multiplication and add the third parameter to the result of multiplication
+        int totalResult = main.Add(main.Multiply(mulParam1, mulParam2), addParam);
+        // Assert that the total result matches the expected result
+        Assertions.assertEquals(expectedResult, totalResult);
+    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/TestData_DivisionAndSubtraction.csv")
+    public void testDivisionAndSubtraction(int subParam1, int subParam2, int divParam, int expectedResult) {
+        // Perform the subtraction
+        int subResult = main.Substract(subParam1, subParam2);
+        // Perform the division with the result of subtraction
+        int divResult = main.Divide(subResult, divParam);
+        // Assert that the result of division matches the expected result
+        Assertions.assertEquals(expectedResult, divResult);
+    }
 }
--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -89,25 +89,7 @@ public class TestCalculator {
         int roundedResult = main.rountvalue(input);
         Assertions.assertEquals(expected, roundedResult);
     }
-    @ParameterizedTest
-    @CsvFileSource(resources = "/TestData_AdditionAndMultiplication.csv")
-    public void testAdditionAndMultiplication(int mulParam1, int mulParam2, int addParam, int expectedResult) {
 
-        // Perform the multiplication and add the third parameter to the result of multiplication
-        int totalResult = main.Add(main.Multiply(mulParam1, mulParam2), addParam);
-        // Assert that the total result matches the expected result
-        Assertions.assertEquals(expectedResult, totalResult);
-    }
-    @ParameterizedTest
-    @CsvFileSource(resources = "/TestData_DivisionAndSubtraction.csv")
-    public void testDivisionAndSubtraction(int subParam1, int subParam2, int divParam, int expectedResult) {
-        // Perform the subtraction
-        int subResult = main.Substract(subParam1, subParam2);
-        // Perform the division with the result of subtraction
-        int divResult = main.Divide(subResult, divParam);
-        // Assert that the result of division matches the expected result
-        Assertions.assertEquals(expectedResult, divResult);
-    }
 
 
 }
