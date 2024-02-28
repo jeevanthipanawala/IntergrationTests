25f1f2606286da9fa9dcc012e4dffdfafaba7041
jeevanthipanawala <jeevanthi.panawala@gmail.com>
2024-02-28 20:06:12 +0100

    Screen Recording - Task 3 - Continuous Integration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import static java.lang.Math.max;
+import static java.lang.Math.*;
 
 //Simple Calculator (Integer Operations) for Continuous Integration Demonstration
 public class Main {
@@ -38,6 +38,36 @@ public class Main {
             return max(param1,param2);
         }
 
+    /*** Calculate Modulus ***/
+    public int Modulus(int param1,int param2)
+    {
+
+        return (param1%param2);
+    }
+
+    /*** Get the squareroot ***/
+    public double Squareroot(int param1)
+    {
+        return sqrt(param1);
+    }
+
+    /*** Get Absolute Value ***/
+    public int AbsoluteValue(int param1)
+    {
+        return abs(param1);
+    }
+
+    /*** Get the minimum value ***/
+    public int MinValue(int param1,int param2)
+    {
+        return min(param1,param2);
+    }
+
+    /*** Round a float to an integer ***/
+    public int RoundValue(float param1)
+    {
+        return round(param1);
+    }
 
     }
 
